f2c9352e26ee54557defb459902a4e5121024d01
whoaz <whoaz@qq.com>
2016-08-16 15:42:39 +0800

    add first page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
